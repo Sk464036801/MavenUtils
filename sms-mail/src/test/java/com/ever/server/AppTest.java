f12f151ce36f62899d05a6663f4c22aa7a664c84
shangke <xiaoshang_ke@sina.cn>
2015-04-04 11:42:49 +0800

    AppTest commit.

--- sms-mail/src/test/java/com/ever/server/AppTest.java
+++ sms-mail/src/test/java/com/ever/server/AppTest.java
@@ -1,14 +1,14 @@
 package com.ever.server;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+        import junit.framework.Test;
+        import junit.framework.TestCase;
+        import junit.framework.TestSuite;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
+        extends TestCase
 {
     /**
      * Create the test case
@@ -35,4 +35,9 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+    public void testMethod(){
+
+    }
+
 }
